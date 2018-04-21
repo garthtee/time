c3a495143f4f1ea068fcbe81abed803706cc103e
Garth Toland <tolandgarth@gmail.com>
2018-04-21 18:18:09 +0800

    Fixed hard-coded URLs.

--- src/app/services/time.service.ts
+++ src/app/services/time.service.ts
@@ -7,12 +7,11 @@ export class TimeService {
   constructor(private http: Http) { }
 
   createTime(time) {
-    return this.http.post('http://localhost:3000/times/add/', time);
+    return this.http.post('/times/add/', time);
   }
 
   getAllTimes() {
-    // return this.http.get('http://localhost:3000/times/all/');
-    return this.http.get('http://localhost:3000/times/all/')
+    return this.http.get('/times/all/')
       .map(res => res.json());
   }
 }
